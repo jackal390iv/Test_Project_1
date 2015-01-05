383fefe899cbb149d7bbda588ee51664ea9008ee
jackal390iv <jonpbiv@gmail.com>
2015-01-05 17:37:28 -0500

    updated

--- Test_Project_1/lib/main.rb
+++ Test_Project_1/lib/main.rb
@@ -16,15 +16,19 @@ end
 
 #this section splits that string into a single integer array. 
 # it then checks this array for the correct length of %10
-  ary = data.split(" ") .map { |s| s.to_i }
-  if ary.size%10 != 0 then
-    puts "ERROR: Incorrect number of integers in data.txt file."
-    exit
-  end 
+ary = data.split(" ") .map { |s| s.to_i }
+if ary.size%10 != 0 then
+  puts "ERROR: Incorrect number of integers in data.txt file."
+  exit
+end 
 
+#this
 require 'matrix'
-matrixA = Matrix.build(5,(ary.size/2)/5) #{ary.slice(ary.size/2)}
-matrixB = Matrix.build((ary.size/2)/5,5)
+matrixA = Matrix.build(5,(ary.size/2)/5) {|row, col| -1 }
+matrixB = Matrix.build((ary.size/2)/5,5) {|row, col| -1 }
+
+ 
+matrixA.to_a.each {|m| puts m.inspect}
 
 #ary.each do |num|
 #puts num
